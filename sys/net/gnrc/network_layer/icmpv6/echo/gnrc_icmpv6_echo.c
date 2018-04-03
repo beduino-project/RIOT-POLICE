4514fbe4f0f91a5236d3dd8dd89318536360df12
Sören Tempel <soeren+git@soeren-tempel.net>
2018-04-03 15:38:18 +0200

    gnrc: convert gnrc_icmpv6_echo to checked program scope

--- sys/net/gnrc/network_layer/icmpv6/echo/gnrc_icmpv6_echo.c
+++ sys/net/gnrc/network_layer/icmpv6/echo/gnrc_icmpv6_echo.c
@@ -15,10 +15,15 @@
 #include "net/gnrc.h"
 
 #include "od.h"
+#include "checkedc.h"
 #include "net/gnrc/icmpv6.h"
 #include "net/gnrc/icmpv6/echo.h"
 #include "net/gnrc/ipv6/hdr.h"
 #include "utlist.h"
+
+#ifdef USE_CHECKEDC
+#pragma BOUNDS_CHECKED ON
+#endif
 
 #define ENABLE_DEBUG    (0)
 #include "debug.h"
@@ -29,18 +34,19 @@
 #endif
 
 gnrc_pktsnip_t *gnrc_icmpv6_echo_build(uint8_t type, uint16_t id, uint16_t seq,
-                                       uint8_t *data, size_t data_len)
+                                       uint8_t *data acount(data_len), size_t data_len)
+    atype(ptr(gnrc_pktsnip_t))
 {
-    gnrc_pktsnip_t *pkt;
-    icmpv6_echo_t *echo;
+    size_t pkt_size = data_len + sizeof(icmpv6_echo_t);
+    ptr(gnrc_pktsnip_t) pkt = gnrc_icmpv6_build(NULL, type, 0, pkt_size);
 
-    if ((pkt = gnrc_icmpv6_build(NULL, type, 0, data_len + sizeof(icmpv6_echo_t))) == NULL) {
+    if (pkt == NULL) {
         return NULL;
     }
 
     DEBUG("icmpv6_echo: Building echo message with type=%" PRIu8 "id=%" PRIu16
           ", seq=%" PRIu16, type, id, seq);
-    echo = (icmpv6_echo_t *)pkt->data;
+    array_ptr(icmpv6_echo_t) echo abyte_count(pkt_size) = (array_ptr(icmpv6_echo_t))pkt->data;
     echo->id = byteorder_htons(id);
     echo->seq = byteorder_htons(seq);
 
@@ -56,11 +62,12 @@ gnrc_pktsnip_t *gnrc_icmpv6_echo_build(uint8_t type, uint16_t id, uint16_t seq,
     return pkt;
 }
 
-void gnrc_icmpv6_echo_req_handle(gnrc_netif_t *netif, ipv6_hdr_t *ipv6_hdr,
-                                 icmpv6_echo_t *echo, uint16_t len)
+void gnrc_icmpv6_echo_req_handle(gnrc_netif_t *netif atype(ptr(gnrc_netif_t)),
+                                 ipv6_hdr_t *ipv6_hdr atype(ptr(ipv6_hdr_t)),
+                                 icmpv6_echo_t *echo abyte_count(len + sizeof(icmpv6_echo_t)),
+                                 uint16_t len)
 {
-    uint8_t *payload = ((uint8_t *)echo) + sizeof(icmpv6_echo_t);
-    gnrc_pktsnip_t *hdr, *pkt;
+    array_ptr(uint8_t) payload acount(len) = ((array_ptr(uint8_t))echo) + sizeof(icmpv6_echo_t);
 
     if ((echo == NULL) || (len < sizeof(icmpv6_echo_t))) {
         DEBUG("icmpv6_echo: echo was NULL or len (%" PRIu16
@@ -68,15 +75,16 @@ void gnrc_icmpv6_echo_req_handle(gnrc_netif_t *netif, ipv6_hdr_t *ipv6_hdr,
         return;
     }
 
-    pkt = gnrc_icmpv6_echo_build(ICMPV6_ECHO_REP, byteorder_ntohs(echo->id),
-                                 byteorder_ntohs(echo->seq), payload,
-                                 len - sizeof(icmpv6_echo_t));
+    ptr(gnrc_pktsnip_t) pkt = gnrc_icmpv6_echo_build(ICMPV6_ECHO_REP, byteorder_ntohs(echo->id),
+                                                     byteorder_ntohs(echo->seq), payload,
+                                                     len - sizeof(icmpv6_echo_t));
 
     if (pkt == NULL) {
         DEBUG("icmpv6_echo: no space left in packet buffer\n");
         return;
     }
 
+    ptr(gnrc_pktsnip_t) hdr = NULL;
     if (ipv6_addr_is_multicast(&ipv6_hdr->dst)) {
         hdr = gnrc_ipv6_hdr_build(pkt, NULL, &ipv6_hdr->src);
     }
@@ -94,11 +102,11 @@ void gnrc_icmpv6_echo_req_handle(gnrc_netif_t *netif, ipv6_hdr_t *ipv6_hdr,
     hdr = gnrc_netif_hdr_build(NULL, 0, NULL, 0);
 
     if (netif != NULL) {
-        ((gnrc_netif_hdr_t *)hdr->data)->if_pid = netif->pid;
+        ((ptr(gnrc_netif_hdr_t))hdr->data)->if_pid = netif->pid;
     }
     else {
         /* ipv6_hdr->dst is loopback address */
-        ((gnrc_netif_hdr_t *)hdr->data)->if_pid = KERNEL_PID_UNDEF;
+        ((ptr(gnrc_netif_hdr_t))hdr->data)->if_pid = KERNEL_PID_UNDEF;
     }
 
     LL_PREPEND(pkt, hdr);
